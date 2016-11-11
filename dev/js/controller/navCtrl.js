b837268b147f84d48a508aa377a33534275340d4
sharoninia <sharoninia.sr@gmail.com>
2016-11-11 14:03:37 -0300

    Disponibilidad

--- dev/js/controller/navCtrl.js
+++ dev/js/controller/navCtrl.js
@@ -14,17 +14,23 @@ function navCtrl($scope, $rootScope, navService){
     };
 
     vm.buscarDisponibilidad = function () {
-      return navService.getParqueoUsuario($rootScope.user).then(function(res) {
-        vm.filter.pId = res.data[0].parqueo;
-        var params = {hD: new Date(vm.filter.hD), hH: new Date(vm.filter.hH), tV: vm.filter.tV, pId : vm.filter.pId};
-        navService.buscarDisponibilidad(params).then(function(data) {
-          console.log(data);
-          vm.filter.puesto = data;
-          $('#search').collapse("hide");
-          $('#searchPanel').collapse("show");
-          $scope.$digest();
+      //if($('#searchForm').$valid){
+        return navService.getParqueoUsuario($rootScope.user).then(function(res) {
+          vm.filter.pId = res.data[0].parqueo;
+          var params = {hD: new Date(vm.filter.hD), hH: new Date(vm.filter.hH), tV: vm.filter.tV, pId : vm.filter.pId};
+          navService.buscarDisponibilidad(params).then(function(data) {
+            if(data){
+              console.log(data);
+              vm.filter.puesto = data;
+              $('#search').collapse("hide");
+              $('#searchPanel').collapse("show");
+              $scope.$digest();
+            }
+          });
+        }, function( err ){
+              console.error(err);
         });
-      });
+      //}
     };
 
     vm.cambiarModoPago = function(){
